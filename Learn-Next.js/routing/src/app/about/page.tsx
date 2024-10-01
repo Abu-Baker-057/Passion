30df1c5367ccc2fda8159447d4a67de3fa30f9ce
Abu Baker Shad <abubakershad057@gmail.com>
2024-10-01 05:56:08 -0700

    Routing Assignment

---
+++ Learn-Next.js/routing/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>About Webpage</div>
+  )
+}
+
+export default about
---
+++ Learn-Next.js/routing/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div>Login Webpage</div>
+  )
+}
+
+export default login
